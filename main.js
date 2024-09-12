d325392368b1454ef41afb9960584b8fd86242ad
najwa <NajwaBuayjan@gmail.com>
2024-09-12 13:32:03 +0300

    solving the lab

---
+++ main.js
@@ -0,0 +1,24 @@
+function changeChannel(num) {
+
+    let imagesList = [
+        "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/blue-planet.jpg",
+        "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/deadpool.jpg",
+        "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/gravity.jpg",
+        "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/good-will-hunting.jpg",
+        "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/cnn.jpg",
+        "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/koyaanisqatsi.jpg",
+        "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/parts-unknown.jpg",
+        "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/star-wars-solo.jpeg",
+        "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/stranger-things.jpg"
+    ];
+
+    let tvFrame = document.getElementById("image");
+        console.log(tvFrame);
+
+    if (imagesList.indexOf(num)) {
+        tvFrame.src = imagesList[num];
+        console.log( imagesList[num]);
+    } else {
+        console.error("No image");
+    }
+}
